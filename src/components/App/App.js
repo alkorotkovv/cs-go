f3f06e471ef75064680f204a125cae290afb614c
Alex <al.korotkov@intranet-sintek.net>
2023-04-10 08:58:08 +0300

    init settings

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import Menu from '../Menu/Menu.js';
+
+function App() {
+  return (
+    <div className="page">
+      <Menu />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import closeLogo from '../../images/background.jpg';
+
+function Menu(props) {
+
+  function handleClick(evt) {
+    if ( (evt.target.classList.contains('menu_visible')) || (evt.target.classList.contains('menu__item')) )
+      handleClose();
+  }
+
+  function handleClose() {
+      props.onClose();
+  }
+
+  return (
+    <div className={"menu" + (props.isOpen ? " menu_visible" : "")} onClick={handleClick} >
+      <ul className={"menu__items" + (props.isOpen ? " menu__items_visible" : "")}>
+        <li className="menu__item"><img className="menu__close" src={closeLogo} alt="закрыть" onClick={props.onClose} /></li>
+        <li className="menu__item"><Link to="/" className="menu__main" title="Главная" onClick={props.onClose}>Главная</Link></li>
+        <li className="menu__item"><Link to="/movies" className="menu__link menu__link_active" title="Фильмы" onClick={props.onClose}>Фильмы</Link></li>
+        <li className="menu__item"><Link to="/saved-movies" className="menu__link" title="Сохраненные фильмы" onClick={props.onClose}>Сохраненные фильмы</Link></li>
+        <li className="menu__item"><Link to="/profile" className="menu__profile" title="Аккаунт" onClick={props.onClose}>Аккаунт</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Menu;
