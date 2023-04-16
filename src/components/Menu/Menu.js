42c46c4a2cbc91038f448005e1770cbc08164699
alkorotkovv <akorotkov95@mail.ru>
2023-04-16 11:43:57 +0300

    + videos

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import menuLogo from '../../images/background.jpg';
 import anubis from '../../movies/anubis.webm';
+import terr from '../../movies/terr.webm';
+import terrgif from '../../movies/giff.gif';
 
 function Menu(props) {
 
-  console.log (anubis);
+  //console.log (terr);
 
   function handleClick(evt) {
   }
 
   return (
     <div className="menu" onClick={handleClick} >
-      <video id="nubexVideo" width="100%" height="auto" loop="loop" autoplay="autoplay" preload="auto">
-        <source src={anubis} type="video/webm"></source>
+      <video id="nubexVideo" width="auto" height="auto" autoPlay muted loop="loop" preload="auto">
+        {/*<source src={anubis} type='video/webm' ></source>*/}
+        {/*<source src={terr} type='video/webm' ></source>*/}
+        <img src={terrgif} alt="я джифка"></img>
       </video>
+      <img src={terrgif} alt="я джифка"></img>
       <img className="menu__background" src={menuLogo} alt="закрыть" onClick={props.onClose} />
     </div>
   )
