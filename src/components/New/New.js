16ddd1d4c1c54a1d9c12475fbf82b0982d425787
Alex <al.korotkov@intranet-sintek.net>
2023-04-21 13:56:09 +0300

    edit news block

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -1,19 +1,16 @@
 import React  from 'react';
 
-import { newsArray } from '../../utils/constants';
-import newsIcon from '../../images/news.png';
-
 function New(props) {
   
   return (
     <div className='new'>
       <img className='new__logo' src={props.logo} alt="новость"/>
-      <h3 className='new__title'>{props.title}</h3>
-      <p className='new__date'>{props.date}</p>
-      <p className='new__text'>{props.text}</p>
-    </div>
-
-    
+      <div className='new__block'>
+        <p className='new__date'>{props.date}</p>
+        <h3 className='new__title'>{props.title}</h3>
+        <p className='new__text'>{props.text}</p>
+      </div>
+    </div>  
   )
 }
 
