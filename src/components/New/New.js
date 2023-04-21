bc72b37e0cb9c3c11a59f205c43397510e4c8a7f
Alex <al.korotkov@intranet-sintek.net>
2023-04-21 14:28:01 +0300

    edit news block

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -4,8 +4,10 @@ function New(props) {
   
   return (
     <div className='new'>
-      <img className='new__logo' src={props.logo} alt="новость"/>
-      <div className='new__block'>
+      <div className='new__logo-block'>
+        <img className='new__logo' src={props.logo} alt="новость"/>
+      </div>
+      <div className='new__article-block'>
         <p className='new__date'>{props.date}</p>
         <h3 className='new__title'>{props.title}</h3>
         <p className='new__text'>{props.text}</p>
