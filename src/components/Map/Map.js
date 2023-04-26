4a3224ea66d47d1de67643e971c28daaadf80c38
Alex <al.korotkov@intranet-sintek.net>
2023-04-26 16:01:30 +0300

    edit map styles

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,9 +2,17 @@ import React  from 'react';
 import competitiveIcon from '../../images/csgo_map_icon.png';
 
 function Map(props) {
+
+  const [isChecked, setIsChecked] = React.useState(false);
+
+  function handleClick() {
+    setIsChecked(!isChecked);
+  }
+
+
   return (
-    <li className='map'>
-      <img className='map__logo' src={props.logo} alt="logo"/>
+    <li className={"map" + (isChecked ? " map_checked" : " ")} onClick={handleClick}>
+      <img className={"map__logo" + (isChecked ? " map__logo_checked" : " ")} src={props.logo} alt="logo"/>
       <img className='map__icon' src={props.icon} alt="icon"/>
       <p className='map__title'>{props.name}</p>
       <p className='map__time'>{props.time}</p>
