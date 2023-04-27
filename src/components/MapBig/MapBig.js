e2b5d375eefcdb15865402b9cf9b397b6a375738
Alex <al.korotkov@intranet-sintek.net>
2023-04-27 15:55:52 +0300

    add logic to big-map

--- src/components/MapBig/MapBig.js
+++ src/components/MapBig/MapBig.js
@@ -7,6 +7,13 @@ function MapBig(props) {
   console.log(props);
 
   const [isChecked, setIsChecked] = React.useState(false);
+  const [isShow, setIsShow] = React.useState(false);
+
+  React.useEffect(() => {
+    setInterval(function() {
+      //console.log("2s")
+    }, 2000)
+  }, [])
 
   function handleClick() {
     setIsChecked(!isChecked);
@@ -15,13 +22,17 @@ function MapBig(props) {
 
   return (
     <li className={"map-big" + (isChecked ? " map-big_checked" : " ")} onClick={handleClick}>
-      <img className={"map-big__logo" + (isChecked ? " map-big__logo_checked" : " ")} src={props.maps[0].logo} alt="logo"/>
+      {
+        props.maps.map((element, index) => 
+          <img className={"map-big__logo" + ` map-big__logo_${index}`} src={element.logo} alt="logo"/>
+        )
+      }
       <ul className='map-big__list'>
         {
           props.maps.map((element, index) => 
             <li key={index} className='map-big__item'><img className='map-big__icon' src={element.icon} alt="icon"/></li>
           )
-        }  
+        }
       </ul>
       <p className='map-big__title'>{props.name}</p>
     </li>
