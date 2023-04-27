93c4b90596e8e5d9bf17613542d8a46dd752344f
Alex <al.korotkov@intranet-sintek.net>
2023-04-27 12:16:36 +0300

    add templates in competitive regym

--- src/components/Play/Regyms/Competitive/Competitive.js
+++ src/components/Play/Regyms/Competitive/Competitive.js
@@ -2,7 +2,11 @@ import React  from 'react';
 import Switch from '../../../Switch/Switch';
 import Map from '../../../Map/Map';
 
+import add_all_icon from '../../../../images/add_all_icon.png';
+import delete_all_icon from '../../../../images/delete_all_icon.png';
+import save_icon from '../../../../images/save_icon.png';
 import { competitiveMapsArray } from '../../../../utils/constants';
+
 
 function Competitive(props) {
   
@@ -15,6 +19,32 @@ function Competitive(props) {
       </div>
       <div className='regym__main regym__main_competitive'>
         <div className='filters'>
+          <div className='filters__templates-block'>
+            <p className='filters__title'>Шаблоны</p>
+            <ul className='filters__templates'>
+              <li className='filters__template'><p className='filters__template-text'>Избранное</p></li>
+              <li className='filters__template'><p className='filters__template-text'>Новые</p></li>
+              <li className='filters__template filters__template_active'><p className='filters__template-text'>Премьер-режим</p></li>
+              <li className='filters__template'><p className='filters__template-text'>Передовая</p></li>
+              <li className='filters__template'><p className='filters__template-text'>Заложники</p></li>
+            </ul>
+          </div>
+          <div className='filters__actions-block'>
+            <ul className='filters__actions'>
+              <li className='filters__action'>
+                <img className='filters__action-icon' src={add_all_icon} alt="icon"/>
+                <p className='filters__action-text'>Выбрать все</p>
+              </li>
+              <li className='filters__action'>
+                <img className='filters__action-icon' src={delete_all_icon} alt="icon"/>
+                <p className='filters__action-text'>Удалить все</p>
+              </li>
+              <li className='filters__action'>
+                <img className='filters__action-icon' src={save_icon} alt="icon"/>
+                <p className='filters__action-text'>Сохранить как ...</p>
+              </li>
+            </ul>
+          </div>
 
         </div>
         <div className='maps'>
