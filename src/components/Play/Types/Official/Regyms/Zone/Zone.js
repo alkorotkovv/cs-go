9d31e05d39c17b227cda915b42c64605dce67ff8
Alex <al.korotkov@intranet-sintek.net>
2023-05-03 09:56:02 +0300

    add team in zone

--- src/components/Play/Types/Official/Regyms/Zone/Zone.js
+++ src/components/Play/Types/Official/Regyms/Zone/Zone.js
@@ -2,8 +2,15 @@ import React  from 'react';
 import Switch from '../../../../../Switch/Switch';
 import { allMapsArray, zoneFriendsArray } from '../../../../../../utils/constants';
 import updateIcon from '../../../../../../images/update_icon.png';
+import timeIcon from '../../../../../../images/time_icon.png';
 
 function Zone(props) {
+
+  const [isChecked, setIsChecked] = React.useState(false);
+
+  function handleClick() {
+    setIsChecked(!isChecked);
+  }
   
   return (
     <div className={"regym regym_zone" + (props.isVisible ? " regym_visible" : " ")}>
@@ -16,6 +23,12 @@ function Zone(props) {
           <div className='zone-map'>
             <img className='zone-map__image' src={allMapsArray.find((e) => e.name === "Sirocco").logo} alt="logo"/>
             <img className='zone-map__icon' src={allMapsArray.find((e) => e.name === "Sirocco").icon} alt="icon"/>
+            <div className='zone-map__time'>
+              <img className='zone-map__time-icon' src={timeIcon} alt="icon"/>
+              <p className='zone-map__time-time'>02.08</p>
+              <p className='zone-map__time-text'>до смены карты на</p>
+              <p className='zone-map__time-map'>Sirocco</p>
+            </div>
           </div>
           <div className='table'>
             <div className='table__header'>
@@ -38,6 +51,11 @@ function Zone(props) {
                 )
               }  
             </ul>
+            <div className='table__team' onClick={handleClick}>
+              <div className={'table__checkbox' + (isChecked ? " " : " table__checkbox_unactive")}></div>
+              <div className={'table__group' + (isChecked ? " " : " table__group_unactive")}></div>
+              <p className={'table__team-text' + (isChecked ? " " : " table__team-text_unactive")}>НАЙТИ КОМАНДУ</p>
+            </div>
           </div>
         </div>
       </div>
