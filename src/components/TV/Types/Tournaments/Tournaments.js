524f12c24cf6f24a7425272ce6599387b3df523f
Alex <al.korotkov@intranet-sintek.net>
2023-06-02 14:49:10 +0300

    add tournaments

--- src/components/TV/Types/Tournaments/Tournaments.js
+++ src/components/TV/Types/Tournaments/Tournaments.js
@@ -1,49 +1,22 @@
 import React  from 'react';
-import Select from '../../../Select/Select';
-import Switch from '../../../Switch/Switch';
-import Map from '../../../Map/Map';
-import { workshopMapsArray } from '../../../../utils/constants';
+import tournaments from '../../../../images/tournaments.png';
 
 function Tournaments(props) {
 
-  const [name, setName] = React.useState("");
-
-  //Обработчик ввода текста в инпут
-  function handleNameChange(evt) {
-    setName(evt.target.value);
-  }
 
   return (
     <div className={"type type_tournaments" + (props.isVisible ? " type_visible" : " ")} >
-      <div className='type__regyms'>
-        
-      </div>
       <div className='type__main'>
-        <div className={"regym regym_workshop" + (props.isVisible ? " regym_visible" : " ")}>
-          <div className='regym__settings regym__settings_workshop'>
-            <div className='wrapper wrapper_workshop'>
-              <Switch span="Открытая группа" id="switch_workshop_1" isChecked={true}/>
-            </div>
-          </div>
-          <div className='regym__main-wrapper'>
-            <div className='regym__main regym__main_workshop'>
-              <ul className="regym__maps regym__maps_workshop">  
-                {
-                  workshopMapsArray.filter((e) => e.name.includes(name)).map((element, index) => 
-                    <Map 
-                      key={index}
-                      logo={element.logo}
-                      icon={element.icon}
-                      name={element.name}
-                      time={element.time}
-                      isCompetitive={element.isCompetitive}
-                      isCheckable={false}
-                      isTimeVisible={false}
-                      isIconVisible={false}
-                    />
-                  )
-                }  
-              </ul>
+        <div className='regym__main-wrapper regym__main-wrapper_transparent'>
+          <div className='regym__secondary regym__secondary_workshop'>
+            <div className='tournaments'>
+              <div className='tournaments__main'>
+                <p className='tournaments__title'>СЕЙЧАС</p>
+                <img className='tournaments__image' src={tournaments} alt="турниры" draggable="false"/>
+              </div>
+              <div className='tournaments__bottom'>
+                <p className='tournaments__bottom-text'>Информация о турнирах предоставлена порталом HLTV.org</p>
+              </div>
             </div>
           </div>
         </div>
